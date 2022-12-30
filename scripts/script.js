3d85076d31203f857fd662e9f7b3af644e076df0
Fulya <fulya.ertay@gmail.com>
2022-12-30 20:54:06 +0300

    feat: create collapsible blogs

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,7 @@
+/* when DOM loaded, init all modals */
 document.addEventListener('DOMContentLoaded',function(){
     let modals=document.querySelectorAll('.modal');
     M.Modal.init(modals);
+    let blogs=document.querySelectorAll('.collapsible');
+    M.Collapsible.init(blogs);
 })
